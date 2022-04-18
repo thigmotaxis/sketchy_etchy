f0f9d1d8d6da8c3c7b854a41f930eb3fb2e94544
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-04-18 11:53:32 -0700

    fix typo which broke the eraser button

--- script.js
+++ script.js
@@ -24,24 +24,9 @@ const sketch = document.querySelector("#sketch");
 sketch.addEventListener("click", () => {
   applyColor();
 });
-
-// create event listener that calls eraseByMouse when "eraser" button is clicked
-const eraser = document.querySelector("#eraser");
-eraser.addEventListener("click", () => {
-  removeSketch();
-  eraseByMouse();
-})
-
-function removeSketch() {
-  const gridDivs = document.querySelectorAll(".gridBox")
-  gridDivs.forEach((gridDiv) => {
-    gridDiv.removeEventListener("mouseover", applyColor);
-  });
-}
-
 // function that removes ".touched" from previously moused over grid boxes
 function eraseByMouse() {
-  const gridDivs = document.querySelectorAll(".gridbox")
+  const gridDivs = document.querySelectorAll(".gridBox")
   gridDivs.forEach((gridDiv) => {
     gridDiv.addEventListener("mouseover", () => {
       gridDiv.classList.remove("touched");
@@ -49,12 +34,28 @@ function eraseByMouse() {
   });
 }
 
+// create event listener that calls eraseByMouse when "eraser" button is clicked
+const eraser = document.querySelector("#eraser");
+eraser.addEventListener("click", () => {
+  eraseByMouse();
+})
+
+// this is suppposed to remove the event listener added to each gridBox when the sketch button is clicked
+function removeSketch() {
+  const gridDivs = document.querySelectorAll(".gridBox")
+  gridDivs.forEach((gridDiv) => {
+    gridDiv.removeEventListener("mouseover", applyColor);
+  });
+}
+
+
 // create an event listener that removes the class "touched" from all divs with the class gridBox when the reset button is pressed
 const boardWipe = document.querySelector("#boardWipe");
 boardWipe.addEventListener("click", () => {
   let gridsToPurge = document.querySelectorAll(".gridBox")
   gridsToPurge.forEach((grid) => {
     grid.classList.remove("touched");
+    removeSketch();
   });
 });
 
